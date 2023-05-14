99138f5e9866bbed4abb2f62737edc87d0c29f39
Kevin Sims <kevinsims@Not-Yours.attlocal.net>
2023-05-14 00:58:24 -0700

    rdy

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,41 @@
 import Navigation from './Navigation'
+import { useState, useCallback, useEffect } from 'react';
 
+const useMediaQuery = (width) => {
+  const [targetReached, setTargetReached] = useState(false);
+
+  const updateTarget = useCallback((e) => {
+    if (e.matches) {
+      setTargetReached(true);
+    } else {
+      setTargetReached(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    const media = window.matchMedia(`(max-width: ${width}px)`);
+    media.addListener(updateTarget);
+
+    // Check on mount (callback is not called until a change occurs)
+    if (media.matches) {
+      setTargetReached(true);
+    }
+
+    return () => media.removeListener(updateTarget);
+  }, []);
+
+  return targetReached;
+};
 export default function Layout({ children }) {
+  const isBreakpoint = useMediaQuery(540)
+  console.log('isBreak', isBreakpoint)
   return (
-    <>
-      <Navigation />
-      <main>{children}</main>
-    </>
+   <>
+    {isBreakpoint? (
+      <> <Navigation />
+      <main>{children}</main> </>
+
+    ) : <>Site Is Optimized For Mobile Viewing - Please Switch to your phone.</>}
+   </>
   )
 }
